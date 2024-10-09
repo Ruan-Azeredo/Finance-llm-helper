56f9e9bde7cc09f35e4ca5c94f9aa159123734b1
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-09 16:44:32 -0300

    :sparkles: feat: errors treatment

--- src/interface/FileManager.py
+++ src/interface/FileManager.py
@@ -5,16 +5,23 @@ import os
 def loadDir(path: str = 'extratos'):
     files = []
 
-    for file in os.listdir(path):
-        files.append(file)
+    try:
+        for file in os.listdir(path):
+            files.append(file)
 
+    except:
+        raise Exception("Nenhum arquivo encontrado")
+    
     return files
 
 
-def loadFile(path: str = 'extratos', file_name: str = 'Extrato-01-09-2024-a-01-10-2024 (1).ofx'):
+def loadOfxFile(path: str = 'extratos', file_name: str = 'Extrato-01-09-2024-a-01-10-2024 (1).ofx'):
+    if file_name.endswith('.ofx'):
+        with open(f'{path}/{file_name}', encoding='ISO-8859-1') as ofx_file:
+            print(ofxparse.OfxParser.parse(ofx_file))
+            parsed_data = ofxparse.OfxParser.parse(ofx_file)
+        
+        return parsed_data
 
-    with open(f'{path}/{file_name}', encoding='ISO-8859-1') as ofx_file:
-        print(ofxparse.OfxParser.parse(ofx_file))
-        parsed_data = ofxparse.OfxParser.parse(ofx_file)
-    
-    return parsed_data
+    else:
+        raise Exception("Arquivo inválido, arquivo deve ser do formato .ofx")

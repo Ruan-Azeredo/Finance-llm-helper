1b77f3ba337e02ef6df9a0d06e276d87e6ebea72
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-09 10:58:55 -0300

    :white_check_mark: test: tests data processing service

---
+++ src/tests/test_DataProcessingService.py
@@ -0,0 +1,24 @@
+import pytest
+from services.DataProcessingService import processedData
+from pTypes import Transaction
+
+def test_data_processing_service_return_transaction():
+
+    data = processedData({
+        'id': '1',
+        'date': '2022-01-01',
+        'amount': 100,
+        'memo': 'teste'
+    })
+    
+    assert type(data) == Transaction
+
+def test_data_processing_service_return_error_if_file_is_incomplete():
+
+    with pytest.raises(Exception) as error:
+        processedData({
+            'id': '1',
+            'amount': 100
+        })
+    
+    assert str(error.value) == "Transação com dados incompletos"

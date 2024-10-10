694f0133e0f2eedc79ba8cf8e37c15c4ea1dd04e
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-10 17:11:34 -0300

    :white_check_mark: test: processing test, and integration test

---
+++ src/tests/test_DataProcessingService.py
@@ -0,0 +1,30 @@
+from src.services import dataProcessingService
+from src.pInterface import loadDataFromOfxFile, loadDir
+from src.useCases import parsedData
+from src.pTypes import Transaction
+
+def test_data_processing_service_function():
+    processed_transaction_list = dataProcessingService('extratos')
+    
+    assert processed_transaction_list != None
+    assert isinstance(processed_transaction_list, list)
+
+    for transaction in processed_transaction_list:
+        assert isinstance(transaction, Transaction)
+
+def test_file_manager_to_data_parser_integration():
+    processed_transaction_list = []
+
+    files = loadDir(path='src/tests/extratos')
+
+    for file in files:
+        data_from_ofx= loadDataFromOfxFile(path='src/tests/extratos', file_name=file)
+        for transaction in data_from_ofx:
+            processed_transaction = parsedData(transaction)
+            processed_transaction_list.append(processed_transaction)
+
+    assert processed_transaction_list != None
+    assert isinstance(processed_transaction_list, list)
+
+    for transaction in processed_transaction_list:
+        assert isinstance(transaction, Transaction)

b7b4aa86c414a7203f54180febfcc0c59e27268d
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-10 19:21:09 -0300

    :construction: build: DataProcessing Service

--- src/services/DataProcessingService.py
+++ src/services/DataProcessingService.py
@@ -1,17 +1,19 @@
-from pTypes import Transaction, FileTransaction
+from src.pInterface import loadDataFromOfxFile, loadDir
+from src.useCases import parsedData
+from src.pTypes import Transaction
 
-def processedData(file_transaction: FileTransaction) -> Transaction:
+def dataProcessingService(path: str):
 
-    try:
-        transaction = Transaction(
-            id = file_transaction["id"],
-            Data = file_transaction["date"],
-            Valor = file_transaction["amount"],
-            Descrição = file_transaction["memo"],
-        )
+    if path is None:
+        Exception("Nenhum caminho informado para a pasta de arquivos")
 
-    except:
-        raise Exception(f"Transação com dados incompletos")
+    processed_transaction_list = []
 
-    return transaction
-        
+    files: list = loadDir(path)
+    for file in files:
+        data_from_ofx: list = loadDataFromOfxFile(path, file)
+        for transaction in data_from_ofx:
+            processed_transaction: Transaction = parsedData(transaction)
+            processed_transaction_list.append(processed_transaction)
+
+    return processed_transaction_list

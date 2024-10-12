380fa1ab790ca5b858646ea794a2329312156535
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-11 23:22:05 -0300

    :recycle: refactor: turn llm service in def

--- src/services/LLMService.py
+++ src/services/LLMService.py
@@ -4,30 +4,27 @@ from langchain_core.prompts import PromptTemplate
 from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
 
-from useCases.PersonalizePrompt import PersonalizePrompt
-from utils.validator import category_validator
-from useCases.LLMAnswerCheck import LLMAnswerCheck
+from src.useCases.PersonalizePrompt import PersonalizePrompt
+from src.useCases.LLMAnswerCheck import LLMAnswerCheck
 
-class LLMService:
+def llmService(prompt_content: str, limit: int = 5) -> str:
+    _  = load_dotenv(find_dotenv())
 
-    def execute(self, prompt_content: str, limit: int = 5) -> str:
-        _  = load_dotenv(find_dotenv())
+    prompt = PersonalizePrompt().execute(prompt_content)
 
-        prompt = PersonalizePrompt().execute(prompt_content)
+    try:
+        chat = ChatGroq(model="llama-3.1-8b-instant")
+        answer = chat.invoke(prompt).content
 
-        try:
-            chat = ChatGroq(model="llama-3.1-8b-instant")
-            answer = chat.invoke(prompt).content
+        is_answer_checked = LLMAnswerCheck().execute(answer)
+        if is_answer_checked:
+            return answer
+        elif limit > 0:
+            llmService(prompt_content, limit - 1)
+        else:
+            return "Erro ao checkar resposta da LLM"
+        
 
-            is_answer_checked = LLMAnswerCheck().execute(answer)
-            if is_answer_checked:
-                return answer
-            elif limit > 0:
-                self.execute(prompt_content, limit - 1)
-            else:
-                return "Erro ao checkar resposta da LLM"
-            
-
-        except Exception as e:
-            print(e)
-            return "Erro ao comunicar-se com a LLM"
+    except Exception as e:
+        print(e)
+        return "Erro ao comunicar-se com a LLM"

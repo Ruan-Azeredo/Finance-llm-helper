149b55ddc767a563ab9c223f7a955d48e5c79668
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-13 00:02:18 -0300

    :white_check_mark: test: test personalize prompt

---
+++ src/tests/test_PersonalizePrompt.py
@@ -0,0 +1,5 @@
+from src.useCases import personalizePrompt
+
+def test_personalizePrompt_returns_string():
+    template = personalizePrompt("Teste", ["Teste"])
+    assert isinstance(template, str)

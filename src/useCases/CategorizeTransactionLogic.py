0ab871c600c967843c18d151574bd67a6c577472
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-13 17:16:07 -0300

    :construction: build: categorize transaction logic

---
+++ src/useCases/CategorizeTransactionLogic.py
@@ -0,0 +1,24 @@
+from utils import category_validator, default_categories as categories
+
+def llmAnswerCheck(answer: str, categories: str = categories):
+    if category_validator(categories, answer):
+        return answer
+    else:
+        return False
+    
+    
+def personalizedPrompt(text: str, categories: str = categories) -> str:
+    template = f"""
+    Você é um analista de dados, trabalhando em um projeto de dados.
+    Seu trabalho é escolher uma categoria adequada para cada transação financeira que vou lhe enviar.
+    Todas são transações financeiras de uma pessoa física.
+
+    Escolha uma dentre as seguintes categorias:
+    {', '.join(categories)}
+
+    Escolha a categoria deste item:
+    {text}
+
+    Responda apenas com a categoria.
+    """
+    return template

16dbfe21ddbf41d406a9c605afd81ab10e1ae81f
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-14 23:46:04 -0300

    :bug: fix: improve refuse category when is none

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -1,5 +1,5 @@
 def category_validator(tags: list, category: str) -> bool:
-    if category in tags:
+    if category in tags and category is not None:
         return True
     else:
         return False

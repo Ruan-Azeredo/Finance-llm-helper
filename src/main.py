4f9fa53006863de0676685f82c423772e9c9796f
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-21 12:31:55 -0300

    :sparkles: feat: all coupled categorize transaction route logic

--- src/main.py
+++ src/main.py
@@ -14,29 +14,36 @@ app = FastAPI()
 async def root():
     return {'message': 'Hello World'}
 
-UPLOD_DIR = "imported-extratos"
+UPLOAD_DIR = "imported-extratos"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-@app.post("categorize-transaction")
-async def categorize_transaction(file: UploadFile = File(...)):
-    allowed_extensions = ['.ofx', '.csv']
-    filename = file.filename
-    file_extension = filename.split('.')[-1].lower()
-
-    if file_extension not in allowed_extensions:
-        raise HTTPException(status_code=400, detail="Arquivo inválido, arquivo deve ser do formato .ofx ou .csv")
-
-    file_path = os.path.join(UPLOD_DIR, filename)
-
+@app.post("/categorizetransaction")
+async def root(file: UploadFile = File(...)):
     try:
-        with open(file_path, "wb") as file:
-            content = await file.read()
-            file.write(content)
-    except Exception as error:
-        raise HTTPException(status_code=500, detail=f"Erro ao salvar o arquivo: {error}")
+        allowed_extensions = ['ofx', 'csv']
+        filename = file.filename
+        file_extension = filename.split('.')[-1].lower()
 
-    return JSONResponse(content={
-        "filename": filename, "message": "File uploaded successfully"
-    })
+        if file_extension not in allowed_extensions:
+            print(file_extension)
+            raise HTTPException(status_code=400, detail="Arquivo inválido, arquivo deve ser do formato .ofx ou .csv")
+
+        file_path = os.path.join(UPLOAD_DIR, filename)
+
+        try:
+            content = await file.read()
+            with open(file_path, "wb") as uploaded_file:
+                uploaded_file.write(content)
+        except Exception as error:
+            raise HTTPException(status_code=500, detail=f"Erro ao salvar o arquivo: {error}")
+
+        return JSONResponse(content={
+            "filename": filename, "message": "File uploaded successfully"
+        })
+
+    except Exception as error:
+        print(error)
+        raise HTTPException(status_code=500, detail=f"Erro inesperado: {error}")
 
 """ transactions: list[Transaction] = dataProcessingService('../extratos-csv')
 

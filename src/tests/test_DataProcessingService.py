88a8848e7e161a7d0b74629db4c29efb54c492b1
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-23 09:32:13 -0300

    :white_check_mark: test: fix data processing service tests

--- src/tests/test_DataProcessingService.py
+++ src/tests/test_DataProcessingService.py
@@ -1,33 +1,50 @@
 from datetime import datetime
+import pytest
+from io import BytesIO
+from fastapi import UploadFile
 
 from services import dataProcessingService
 from interface import loadDataFromOfxFile, loadDir
 from useCases import parsedDataToTransaction
-from pTypes import Transaction
+from pTypes import Transaction, FileTransaction
 
-def test_data_processing_service_function():
-    processed_transaction_list = dataProcessingService('tests/extratos')
-    
-    assert processed_transaction_list != None
-    assert isinstance(processed_transaction_list, list)
+@pytest.mark.asyncio
+async def test_data_processing_service_function():
+    with open('tests/extratos/Extrato-01-09-2024-a-01-10-2024 (1).ofx', 'rb') as file:
+        file_bytes = file.read()
+        upload_file = UploadFile(filename="Extrato-01-09-2024.ofx", file=BytesIO(file_bytes))
 
-    for transaction in processed_transaction_list:
+        data = await dataProcessingService(file = upload_file)
 
-        transaction_description = transaction.split(' | ')
-        assert isinstance(datetime.strptime(transaction_description[0], '%d/%m/%Y'), datetime)
-        assert isinstance(transaction_description[1], str)
-        assert 'R$' in transaction_description[2]
+        processed_transaction_list = data["processed_transaction_list"]
+        transactions_params_list = data["transactions_params_list"]
+        
+        assert processed_transaction_list != None
+        assert transactions_params_list != None
 
-def test_file_manager_to_data_parser_integration():
+        assert isinstance(processed_transaction_list, list)
+        assert isinstance(transactions_params_list, list)
+
+        for transaction in processed_transaction_list:
+
+            transaction_description = transaction.split(' | ')
+            assert isinstance(datetime.strptime(transaction_description[0], '%d/%m/%Y'), datetime)
+            assert isinstance(transaction_description[1], str)
+            assert 'R$' in transaction_description[2]
+
+@pytest.mark.asyncio
+async def test_file_manager_to_data_parser_integration():
     processed_transaction_list = []
 
     files = loadDir(path='tests/extratos')
 
     for file in files:
-        data_from_ofx= loadDataFromOfxFile(path='tests/extratos', file_name=file)
-        for transaction in data_from_ofx:
-            processed_transaction = parsedDataToTransaction(transaction)
-            processed_transaction_list.append(processed_transaction)
+        with open(f'tests/extratos/{file}', 'rb') as file:
+            file_bytes = file.read()
+            data_from_ofx = loadDataFromOfxFile(content = file_bytes)
+            for transaction in data_from_ofx:
+                processed_transaction = parsedDataToTransaction(transaction)
+                processed_transaction_list.append(processed_transaction)
 
     assert processed_transaction_list != None
     assert isinstance(processed_transaction_list, list)

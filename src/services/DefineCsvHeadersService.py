55abe3e274ce9dea8d67dc0ffe42b0cbbdfa22b5
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-22 23:34:51 -0300

    :wrench: chore: await llmInterface

--- src/services/DefineCsvHeadersService.py
+++ src/services/DefineCsvHeadersService.py
@@ -4,10 +4,10 @@ from utils import default_categories as categories
 
 import json
 
-def defineCsvHeadersService(csv_data: str) -> str:
+async def defineCsvHeadersService(csv_data: str) -> str:
 
     prompt = personalizedPrompt(csv_data)
 
-    headers_str = llmInterface(prompt, llmAnswerCheck, callLLM)
+    headers_str = await llmInterface(prompt, llmAnswerCheck, callLLM)
 
     return json.loads(headers_str)

dc900bb3fadec462b9344edd17615d1afd05bf4a
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-23 11:08:20 -0300

    :white_check_mark: test: update tests  acrding to file managers def changes

--- src/tests/test_FileManager.py
+++ src/tests/test_FileManager.py
@@ -1,6 +1,8 @@
 import pytest
 import inspect
 from unittest.mock import mock_open, patch
+from io import BytesIO
+from fastapi import UploadFile
 
 from pTypes import FileTransaction
 from interface import loadDataFromOfxFile, loadDataFromCsvFile, loadDir
@@ -24,55 +26,30 @@ def test_loadDir_when_no_files():
 
 
 def test_loadDataFromOfxFile_returns_file_transactions_data():
-    transactions_data = loadDataFromOfxFile(
-        path='tests/extratos',
-        file_name='Extrato-01-09-2024-a-01-10-2024 (1).ofx'
-    )
+    with open(f'tests/extratos/Extrato-01-09-2024-a-01-10-2024 (1).ofx', 'rb') as file:
+        file_bytes = file.read()
 
-    assert isinstance(transactions_data, list)
+        transactions_data = loadDataFromOfxFile(content = file_bytes)
 
-    # Get Props existents in FileTransaction
-    allowed_keys = {
-        param.name 
-        for param in inspect.signature(FileTransaction.__init__).parameters.values()
-        if param.name != 'self'
-    }
-    
-    for transaction in transactions_data:
+        assert isinstance(transactions_data, list)
 
-        # Just get Props existents in FileTransaction
-        filtered_transaction = {key: value for key, value in transaction.items() if key in allowed_keys}
+        # Get Props existents in FileTransaction
+        allowed_keys = {
+            param.name 
+            for param in inspect.signature(FileTransaction.__init__).parameters.values()
+            if param.name != 'self'
+        }
+        
+        for transaction in transactions_data:
 
-        transactionClass = FileTransaction(**filtered_transaction)
-        assert isinstance(transactionClass, FileTransaction)
+            # Just get Props existents in FileTransaction
+            filtered_transaction = {key: value for key, value in transaction.items() if key in allowed_keys}
 
+            transactionClass = FileTransaction(**filtered_transaction)
+            assert isinstance(transactionClass, FileTransaction)
 
-def test_loadDataFromOfxFile_when_file_is_not_ofx():
-
-    with pytest.raises(Exception) as error:
-        loadDataFromOfxFile(
-            path='src/tests/extratos',
-            file_name='Extrato.pdf'
-        )
-
-    assert str(error.value) == "Arquivo inválido, arquivo deve ser do formato .ofx"
-
-
-def test_loadDataFromOfxFile_when_file_not_found():
-
-    file_name = 'abacaxi.ofx'
-    path = 'src/tests/extratos'
-
-    with pytest.raises(Exception) as error:
-        loadDataFromOfxFile(
-            path=path,
-            file_name=file_name
-        )
-
-    assert str(error.value) == f"Arquivo {file_name} não encontrado no caminho {path}"
-
-
-def test_loadDataFromCsvFile_returns_file_transactions_data():
+@pytest.mark.asyncio
+async def test_loadDataFromCsvFile_returns_file_transactions_data():
     mock_csv_data_list = [
         {
             'csv_data': "Data Lançamento;Valor;Histórico;Descrição\n01/01/2023;100,00;Payment;Salary\n02/01/2023;200,50;Refund;Product",
@@ -132,50 +109,32 @@ Data Lançamento;Histórico;Descrição;Valor;Saldo
     ]
 
     for mock_csv_data in mock_csv_data_list:
-        with patch('builtins.open', mock_open(read_data=mock_csv_data['csv_data'])):
+        file_content = mock_csv_data['csv_data'].encode('utf-8')
             
-            transactions_data = loadDataFromCsvFile('extratos', 'extrato.csv', headers=mock_csv_data['headers'])
-            assert isinstance(transactions_data, list)
+        transactions_data = await loadDataFromCsvFile(content = file_content, headers = mock_csv_data['headers'])
+        assert isinstance(transactions_data, list)
 
-            # Get Props existents in FileTransaction
-            allowed_keys = {
-                param.name 
-                for param in inspect.signature(FileTransaction.__init__).parameters.values()
-                if param.name != 'self'
-            }
-            
-            for transaction in transactions_data:
+        # Get Props existents in FileTransaction
+        allowed_keys = {
+            param.name 
+            for param in inspect.signature(FileTransaction.__init__).parameters.values()
+            if param.name != 'self'
+        }
+        
+        for transaction in transactions_data:
 
-                # Just get Props existents in FileTransaction
-                filtered_transaction = {key: value for key, value in transaction.items() if key in allowed_keys}
+            # Just get Props existents in FileTransaction
+            filtered_transaction = {key: value for key, value in transaction.items() if key in allowed_keys}
 
-                transactionClass = FileTransaction(**filtered_transaction)
-                assert isinstance(transactionClass, FileTransaction)
+            transactionClass = FileTransaction(**filtered_transaction)
+            assert isinstance(transactionClass, FileTransaction)
 
+@pytest.mark.asyncio
+async def test_loadDataFromCsvFile_when_data_is_empty():
 
-def test_loadDataFromCsvFile_when_file_is_not_csv():
-
-    with pytest.raises(Exception) as error:
-        loadDataFromCsvFile('extratos', 'extrato.pdf')
-
-    assert str(error.value) == "Arquivo inválido, arquivo deve ser do formato .csv"
-
-
-def test_loadDataFromCsvFile_when_file_not_found():
-
-    with patch('builtins.open', side_effect=FileNotFoundError):
-        with pytest.raises(Exception) as error:
-            loadDataFromCsvFile('extratos', 'extrato.csv')
-
-        assert str(error.value) == "Arquivo extrato.csv não encontrado no caminho extratos"
-
-
-def test_loadDataFromCsvFile_when_data_is_empty():
-
-    empty_csv_data = "Data Lançamento;Valor;Histórico;Descrição\n"
+    empty_csv_data = "Data Lançamento;Valor;Histórico;Descrição\n".encode('utf-8')
     
-    with patch('builtins.open', mock_open(read_data=empty_csv_data)):
-        with pytest.raises(Exception) as error:
-            loadDataFromCsvFile('extratos', 'extrato.csv')
+    with pytest.raises(Exception) as error:
+        await loadDataFromCsvFile(content = empty_csv_data)
 
-        assert str(error.value) == "O arquivo CSV parece estar vazio"
+    assert str(error.value) == "O arquivo CSV parece estar vazio"

8faee51a66bbdb96b1af43c17fe76881b9d295ba
Ruan-M2 <ruanazeredo@gmail.com>
2024-10-22 23:34:00 -0300

    :recycle: refactor: process file content, read file before call file interface

--- src/services/DataProcessingService.py
+++ src/services/DataProcessingService.py
@@ -3,26 +3,29 @@ from useCases import parsedDataToTransaction, formatDescriptionTransaction
 from services import defineCsvHeadersService
 from pTypes import FileTransaction
 
-def dataProcessingService(path: str):
+from fastapi import File, UploadFile
 
-    if path is None:
-        Exception("Nenhum caminho informado para a pasta de arquivos")
+async def dataProcessingService(file: UploadFile = File(...)):
 
     processed_transaction_list = []
 
-    files: list = loadDir(path)
-    for file in files:
-        if file.endswith('.ofx'):
-            data_from_ofx: list[FileTransaction] = loadDataFromOfxFile(path, file)
-        elif file.endswith('.csv'):
-            file_data = openCsvFile(path, file)
-            headers = defineCsvHeadersService(file_data)
-            data_from_ofx: list[FileTransaction] = loadDataFromCsvFile(path, file, headers)
-        else:
-            raise Exception("Formato de arquivo inválido, use .ofx ou .csv")
-        
-        for transaction in data_from_ofx:
-            processed_transaction = formatDescriptionTransaction(transaction)
-            processed_transaction_list.append(processed_transaction)
+    if file.filename.endswith('.ofx'):
+        content = await file.read()
+        print('content: ',content)
+        data_from_ofx: list[FileTransaction] = loadDataFromOfxFile(content)
+    elif file.filename.endswith('.csv'):
+        content = await file.read()
+        print('content: ',content)
+        headers = await defineCsvHeadersService(content)
+        data_from_ofx: list[FileTransaction] = await loadDataFromCsvFile(content, headers)
+    else:
+        raise Exception("Formato de arquivo inválido, use .ofx ou .csv")
+    
+    for transaction in data_from_ofx:
+        processed_transaction = formatDescriptionTransaction(transaction)
+        processed_transaction_list.append(processed_transaction)
 
-    return processed_transaction_list
+    return {
+        "processed_transaction_list": processed_transaction_list,
+        "transactions_params_list": data_from_ofx
+    }

35866dbb685e0a4535867d62210a722df389f3fc
Ruan-M2 <ruanazeredo@gmail.com>
2024-11-01 10:59:25 -0300

    :wrench: chore: build dependencies to setup db and tables

---
+++ src/initialize_db.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from peewee import SqliteDatabase
+
+from models import User
+from database import db
+
+def initialize_db():
+    if Path('database.db').exists():
+        db.connect()
+        db.create_tables([User])
+
+        print('Banco de dados criado com sucesso!')
+    else:
+        print('Banco de dados ja existe!')
+
+if __name__ == '__main__':
+    initialize_db()

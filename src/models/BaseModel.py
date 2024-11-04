a10a1d3c23e2971c2fe057e7f903f8d5ce1c5d4d
Ruan-M2 <ruanazeredo@gmail.com>
2024-11-04 16:32:53 -0300

    :recycle: refactor: turn errors treatments into decorator in base model

--- src/models/BaseModel.py
+++ src/models/BaseModel.py
@@ -1,8 +1,23 @@
 from peewee import Model, DoesNotExist, OperationalError
-from typing import Type, TypeVar, List
+from typing import Type, TypeVar, List, Callable
 from database import db
 
 T = TypeVar('T', bound='BaseModel')
+
+def handle_database_error(method: Callable[..., T]) -> Callable[..., T]:
+    def wrapper(*args, **kwargs):
+        try:
+            return method(*args, **kwargs)
+        except OperationalError as error:
+            if 'no such table' in str(error):
+                raise Exception(f"Tabela {args[0]._meta.table_name} não existe")
+            else:
+                raise Exception(f"Erro interno: {error}")
+        except DoesNotExist:
+            return None
+        except Exception as error:
+            raise Exception(f"Erro interno: {error}")
+    return wrapper
 
 class BaseModel(Model):
     class Meta:
@@ -12,72 +27,24 @@ class BaseModel(Model):
         return {field.name: getattr(self, field.name) for field in self._meta.sorted_fields}
     
     @classmethod
+    @handle_database_error
     def all(cls: Type[T]) -> List[T]:
-
-        """ if not cls._meta.database.table_exists(cls._meta.database.table_name):
-            raise Exception(f"A tabela {cls._meta.database.table_name} não existe") """
-
-        try:
-            return list(cls.select())
-        
-        except OperationalError as error:
-            if 'no such table' in str(error):
-                raise Exception(f"Tabela {cls._meta.table_name} não existe")
-            else:
-                raise Exception(f"Erro ao Listar o registro: {error}")
-        except Exception as error:
-            raise Exception(f"Erro ao listar os registros: {error}")
+        return list(cls.select())
 
     @classmethod
+    @handle_database_error
     def from_id(cls: Type[T], id: int) -> T:
-
-        try:
-            return cls.get(cls.id == id)
-        
-        except OperationalError as error:
-            if 'no such table' in str(error):
-                raise Exception(f"Tabela {cls._meta.table_name} não existe")
-            else:
-                raise Exception(f"Erro ao criar o registro: {error}")
-        except DoesNotExist:
-            return None
+        return cls.get(cls.id == id)
 
     @classmethod
+    @handle_database_error
     def create(cls: Type[T], **kwargs) -> T:
+        return super().create(**kwargs)
 
-        try:
-            return super().create(**kwargs)
-        
-        except OperationalError as error:
-            if 'no such table' in str(error):
-                raise Exception(f"Tabela {cls._meta.table_name} não existe")
-            else:
-                raise Exception(f"Erro ao criar o registro: {error}")
-        except Exception as error:
-            raise Exception(f"Erro ao criar o registro: {error}")
-  
+    @handle_database_error
     def update(self: T, **kwargs) -> None:
+        super(BaseModel, self).update(**kwargs).where(self.__class__.id == self.id).execute()
 
-        try:
-            super(BaseModel, self).update(**kwargs).where(self.__class__.id == self.id).execute()
-
-        except OperationalError as error:
-            if 'no such table' in str(error):
-                raise Exception(f"Tabela {self._meta.table_name} não existe")
-            else:
-                raise Exception(f"Erro ao atualizar o registro: {error}")
-        except Exception as error:
-            raise Exception(f"Erro ao atualizar o registro: {error}")
-
+    @handle_database_error
     def delete(self: T) -> None:
-
-        try:
-            super(BaseModel, self).delete().where(self.__class__.id == self.id).execute()
-
-        except OperationalError as error:
-            if 'no such table' in str(error):
-                raise Exception(f"Tabela {self._meta.table_name} não existe")
-            else:
-                raise Exception(f"Erro ao deletar o registro: {error}")
-        except Exception as error:
-            raise Exception(f"Erro ao deletar o registro: {error}")    
+        super(BaseModel, self).delete().where(self.__class__.id == self.id).execute() 

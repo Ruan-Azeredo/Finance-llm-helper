3e202ffe0acc37abef296577d6788efd2833a1dc
Ruan-M2 <ruanazeredo@gmail.com>
2024-11-04 16:19:29 -0300

    :white_check_mark: test: add tests in errors cases in user model

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,4 +1,5 @@
 from peewee import SqliteDatabase
+import pytest
 
 from models import User
 
@@ -137,3 +138,97 @@ def test_delete_user_model():
     user.delete()
     
     assert not User.select().where(User.id == 12).exists()
+
+def test_user_table_exists():
+
+    test_db = setupTestDatabase()
+    
+    test_db.connect()
+    test_db.create_tables([User])
+    
+    assert test_db.table_exists('users')
+
+def test_user_table_not_exists():
+
+    test_db = setupTestDatabase()
+    
+    test_db.connect()
+    
+    assert not test_db.table_exists('users')
+
+def test_get_all_users_when_table_not_exists():
+
+    test_db = setupTestDatabase()
+    
+    test_db.connect()
+
+    user = User
+
+    with pytest.raises(Exception) as error:
+        user.all()
+    
+    assert test_db.table_exists('users') == False
+    assert 'Tabela users não existe' in str(error.value)
+
+def test_get_user_when_table_not_exists():
+
+    test_db = setupTestDatabase()
+    
+    test_db.connect()
+
+    user = User
+
+    with pytest.raises(Exception) as error:
+        user.from_id(id = 1)
+    
+    assert test_db.table_exists('users') == False
+    assert 'Tabela users não existe' in str(error.value)
+
+def test_user_create_user_when_table_not_exists():
+
+    test_db = setupTestDatabase()
+    
+    test_db.connect()
+
+    with pytest.raises(Exception) as error:
+        User.create(
+            name = 'name',
+            email = 'email@email.com',
+            password = 'password'
+        )
+    
+    assert test_db.table_exists('users') == False
+    assert 'Tabela users não existe' in str(error.value)
+
+def test_user_update_user_when_table_not_exists():
+
+    test_db = setupTestDatabase()
+    
+    test_db.connect()
+
+    user = User()
+
+    with pytest.raises(Exception) as error:
+        user.update(
+            id = 1,
+            name = 'name',
+            email = 'email@email.com',
+            password = 'password'
+        )
+    
+    assert test_db.table_exists('users') == False
+    assert 'Tabela users não existe' in str(error.value)
+
+def test_user_delete_user_when_table_not_exists():
+
+    test_db = setupTestDatabase()
+    
+    test_db.connect()
+
+    user = User()
+
+    with pytest.raises(Exception) as error:
+        user.delete()
+    
+    assert test_db.table_exists('users') == False
+    assert 'Tabela users não existe' in str(error.value)

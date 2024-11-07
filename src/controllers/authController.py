b622895ae9812e67fbead272b2896d3d6bcdc9fd
Ruan Ubunto <ruanazeredo@gmail.com>
2024-11-07 07:47:23 -0300

    :bug: fix: type difference between user.pass and user_input.pass

--- src/controllers/authController.py
+++ src/controllers/authController.py
@@ -1,19 +1,20 @@
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 from auth import verify_password, create_access_token
 from models import User
 
-router = APIRouter()
+auth_router = APIRouter()
 
-class UserInput:
+class UserInput(BaseModel):
     email: str
     password: str
 
 
-@router.post("/login")
+@auth_router.post("/login")
 async def login(user_input: UserInput):
     user = User.get_user_by_email(user_input.email)
 
-    if not user or not verify_password(user_input.password, user.password):
+    if not user or not int(user.password) == hash(user_input.password):
         raise HTTPException(status_code = 400, detail = "Email ou senha inválidos")
 
     access_token = create_access_token(data = {"sub": user.email})

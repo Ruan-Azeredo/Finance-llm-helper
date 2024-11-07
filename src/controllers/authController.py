9d8e37750de2a429f6cb1712d1a635f0ba2cfabd
Ruan Ubunto <ruanazeredo@gmail.com>
2024-11-07 16:03:01 -0300

    :goal_net: fix: error handler and turn hash pass to str

--- src/controllers/authController.py
+++ src/controllers/authController.py
@@ -1,5 +1,7 @@
+from peewee import DoesNotExist
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+
 from auth import verify_password, create_access_token
 from models import User
 
@@ -12,10 +14,13 @@ class UserInput(BaseModel):
 
 @auth_router.post("/login")
 async def login(user_input: UserInput):
-    user = User.get_user_by_email(user_input.email)
+    try:
+        user = User.get_user_by_email(user_input.email)
+    except DoesNotExist:
+        raise HTTPException(status_code = 400, detail = "Email ou senha inválido")
 
-    if not user or not int(user.password) == hash(user_input.password):
-        raise HTTPException(status_code = 400, detail = "Email ou senha inválidos")
+    if not user or not user.password == str(hash(user_input.password)):
+        raise HTTPException(status_code = 400, detail = "Email ou senha inválido")
 
     access_token = create_access_token(data = {"sub": user.email})
 

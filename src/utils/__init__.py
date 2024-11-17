778b1eacb7dbafb77da05fac8e688bb8d7a8d6ae
Ruan-M2 <ruanazeredo@gmail.com>
2024-11-16 23:25:05 -0300

    :wrench: chore: init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,4 @@
 from .validator import category_validator
 from .constants import default_categories, default_headers
 from .formater import formatAmount, formatDate, formatHaderKey
+from .setupDatabaseFile import setupDatabaseFileWithUserTable

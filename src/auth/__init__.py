efd86e12edfd7246643dd00f6fc78b8392c38d26
Ruan-M2 <ruanazeredo@gmail.com>
2024-11-16 23:21:01 -0300

    :recycle: refactor: just use custom auth passwrd bearer in utils

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,3 +1,3 @@
 from .config import JWT_SECRET, JWT_ALGORITHM, JWT_ACCESS_TOKEN_EXPIRE_MINUTES
-from .security import verify_password, create_access_token
-from .utils import get_current_user
+from .security import Security
+from .utils import oauth2_scheme
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,42 +1,17 @@
-from auth import JWT_SECRET, JWT_ALGORITHM
-from models import User
-
-from fastapi import Depends, HTTPException, status, Request
+from fastapi import HTTPException, status, Request
 from typing import Optional
 from fastapi.security import OAuth2PasswordBearer
-import jwt
-from jwt import PyJWTError
-
-credentials_exception = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Não foi possivel validar as credenciais",
-    headers={"WWW-Authenticate": "Bearer"},
-)
 
 class CustomOAuth2PasswordBearer(OAuth2PasswordBearer):
     async def __call__(self, request: Request) -> Optional[str]:
         try:
             return await super().__call__(request)
-        except Exception:
-            raise credentials_exception
+        except Exception as error:
+            print(f"Erro ao obter token: {error}")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Não foi possivel validar as credenciais",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
 
 oauth2_scheme = CustomOAuth2PasswordBearer(tokenUrl="login")
-
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-
-    except PyJWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Sua sessão expirou, faca login novamente",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    
-    user = User.get_user_by_email(email)
-    if user is None:
-        raise credentials_exception
-    return user

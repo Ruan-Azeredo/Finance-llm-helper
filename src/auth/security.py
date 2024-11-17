c1e5c0f5719cab6e7ccd3e107ed0ce6eaec7cb5f
Ruan-M2 <ruanazeredo@gmail.com>
2024-11-16 23:19:39 -0300

    :recycle: refactor: security, turn into a class and change call

--- src/auth/security.py
+++ src/auth/security.py
@@ -3,16 +3,19 @@ from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
 from .config import JWT_SECRET, JWT_ALGORITHM, JWT_ACCESS_TOKEN_EXPIRE_MINUTES
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+class Security:
 
-def verify_password(plain_password, hashed_password):
-    if hashed_password == str(hash(plain_password)):
-        return True
-    return False
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def create_access_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES))
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
-    return encoded_jwt
+    def encrypt_password(password: str) -> str:
+        return Security.pwd_context.hash(password)
+
+    def verify_password(plain_password, hashed_password) -> bool:
+        return Security.pwd_context.verify(plain_password, hashed_password)
+
+    def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
+        to_encode = data.copy()
+        expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES))
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
+        return encoded_jwt

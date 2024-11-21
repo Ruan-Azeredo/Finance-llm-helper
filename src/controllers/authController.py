c842e5b6128830c862e1184870caf26a7e4732a2
Ruan-M2 <ruanazeredo@gmail.com>
2024-11-20 22:47:16 -0300

    :sparkles: feat: get access token route

--- src/controllers/authController.py
+++ src/controllers/authController.py
@@ -1,5 +1,5 @@
 from peewee import DoesNotExist
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request, status
 
 from auth import Security
 from models import User
@@ -7,10 +7,12 @@ from schemas import LoginInput
 
 auth_router = APIRouter()
 
-email_or_password_invalid = HTTPException(status_code = 400, detail = "Email ou senha inválido")
 
 @auth_router.post("/login")
 async def login(user_input: LoginInput):
+    
+    email_or_password_invalid = HTTPException(status_code = 400, detail = "Email ou senha inválido")
+    
     try:
         user: User = User.get_user_by_email(user_input.email)
     except DoesNotExist:
@@ -22,6 +24,32 @@ async def login(user_input: LoginInput):
     ):
         raise email_or_password_invalid
 
-    access_token = Security.create_access_token(data = {"sub": user.email})
+    access_token = Security.create_jwt_token(data = {"sub": user.email})
+    refresh_token = Security.create_jwt_token(data = {"sub": user.email})
 
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {
+        "access_token": { "token": access_token, "token_type": "bearer" },
+        "refresh_token": { "token": refresh_token, "token_type": "bearer" }
+    }
+
+@auth_router.get("/get-access-token")
+async def get_access_token(request: Request):
+
+    try:
+        payload = Security.validate_token(token = request.headers["Authorization"], type = "refresh")
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(
+                status_code = status.HTTP_401_UNAUTHORIZED,
+                detail = "Token inválido"
+            )
+
+        if payload:
+            access_token = Security.create_jwt_token(data = {"sub": email}, type = "access")
+            return {"access_token": access_token, "token_type": "bearer"}
+    except Exception as error:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = str(error)
+        )
+    

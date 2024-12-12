da5e5fdb3326fb6bf8235ca656243c34f92c2f65
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-12 11:39:33 -0300

    :construction: build: transaction model features

--- src/models/Transaction.py
+++ src/models/Transaction.py
@@ -13,6 +13,7 @@ class Transaction(BaseModel):
     amount = CharField()
     memo = CharField()
     user_id = ForeignKeyField(User, field='id', backref='transactions', on_delete='CASCADE')
+    tag = CharField(default = None, null = True)
     created_at = DateTimeField(default = datetime.now())
     updated_at = DateTimeField(default = datetime.now())
 
@@ -21,7 +22,7 @@ class Transaction(BaseModel):
         table_name = 'transactions'
 
     def __str__(self):
-        return f'Transaction: {self.id}, {self.date}, {self.amount}, {self.memo}, {self.user_id}'
+        return f'Transaction: {self.id}, {self.date}, {self.amount}, {self.memo}, {self.tag}, {self.user_id}'
 
     @handle_database_error
     def create(user_id: int, **kwargs) -> 'Transaction':
@@ -39,4 +40,31 @@ class Transaction(BaseModel):
         values = handle_values(kwargs)
         values['user_id'] = user_id
 
-        return super(Transaction, Transaction).create(**values)
+        created_transaction = super(Transaction, Transaction).create(**values)
+
+        return created_transaction
+    
+    @handle_database_error
+    def update(self, **kwargs) -> None:
+
+        if 'user_id' in kwargs:
+            raise Exception("O campo 'user_id' não pode ser alterado")
+
+        values = handle_values(kwargs)
+
+        super(Transaction, self).update(**values)
+
+    @handle_database_error
+    def get_transactions_by_user_id(user_id: int) -> list['Transaction']:
+
+        try:
+            User.get_by_id(user_id)
+        except Exception:
+            raise DoesNotExist(f"Usuario com id {user_id} nao encontrado")
+
+        return Transaction.select().where(Transaction.user_id == user_id)
+    
+    @handle_database_error
+    def update_tag(self, tag: str) -> None:
+        self.update(tag = tag)
+    

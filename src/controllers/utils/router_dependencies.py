ea917241631c192a49ba9757b5ea45c56957d5e4
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-16 18:27:26 -0300

    :construction: build: verify if current transaction are from current user

--- src/controllers/utils/router_dependencies.py
+++ src/controllers/utils/router_dependencies.py
@@ -1,14 +1,34 @@
 from fastapi import Depends, HTTPException, status
 from typing import Optional
 
-from models import User
+from models import User, Transaction
 
 def verify_only_self_access_user(current_user: User = Depends(User.get_current_user), user_id: Optional[int] = None) -> bool:
 
     if current_user.role != "admin" and user_id is not None and current_user.id != user_id:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="Acesso negado: não possui permissão para acessar esse usuário"
+            detail="Acesso negado: não possui permissão para acessar esse dados de outro usuário"
+        )
+
+    return True
+
+def verify_only_self_access_transaction(current_user: User = Depends(User.get_current_user), transaction_id: Optional[str] = None) -> bool:
+
+    transaction: Transaction = Transaction.from_id(transaction_id)
+
+    if transaction is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Transação nao encontrada"
+        )
+
+    user: User = User.from_id(transaction.user_id)
+
+    if current_user.role != "admin" and user.id is not None and current_user.id != user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Acesso negado: não possui permissão para acessar esse dados de outro usuário"
         )
 
     return True

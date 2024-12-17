7831df31d34214db68411a5d520e117700c43950
Ruan-M2 <ruanazeredo@gmail.com>
2024-12-16 23:21:16 -0300

    :construction: fix: use setupDatabaseFileWithTables into llm test

--- src/tests/test_e2e_categorize_transaction.py
+++ src/tests/test_e2e_categorize_transaction.py
@@ -1,20 +1,24 @@
 import pytest
 from fastapi.testclient import TestClient
+
 from src.server import app
+from utils import setupDatabaseFileWithTables
 
 client = TestClient(app)
 
 @pytest.mark.llm
 @pytest.mark.e2e
 @pytest.mark.asyncio
-async def test_categorize_transaction_e2e_csv():
+@setupDatabaseFileWithTables(client_test = client, is_admin = True)
+async def test_categorize_transaction_e2e_csv(authenticated_client: TestClient):
     file_path = "tests/extratos-csv/Extrato-01-09-2024-a-01-10-2024 (1).csv"
 
     with open(file_path, 'rb') as file:
-        response = client.post('/categorize-transaction', files={'file': file})
+        response = authenticated_client.post('/categorize-transaction/by-file', files={'file': file})
 
-    assert response.status_code == 200
+    assert 'message' in response.json()
     assert response.json()['message'] == "Transações categorizadas com sucesso"
+    assert response.status_code == 200
     assert len(response.json()['transactions']) > 0
     assert len(response.json()['transactions'][0]['category']) > 0
     assert len(response.json()['transactions'][0]['memo']) > 0
@@ -24,17 +28,18 @@ async def test_categorize_transaction_e2e_csv():
 @pytest.mark.llm
 @pytest.mark.e2e
 @pytest.mark.asyncio
-async def test_categorize_transaction_e2e_ofx():
+@setupDatabaseFileWithTables(client_test = client, is_admin = True)
+async def test_categorize_transaction_e2e_ofx(authenticated_client: TestClient):
     file_path = "tests/extratos/Extrato-01-09-2024-a-01-10-2024 (1).ofx"
 
     with open(file_path, 'rb') as file:
-        response = client.post('/categorize-transaction', files={'file': file})
+        response = authenticated_client.post('/categorize-transaction/by-file', files={'file': file})
 
-    assert response.status_code == 200
+    assert 'message' in response.json()
     assert response.json()['message'] == "Transações categorizadas com sucesso"
+    assert response.status_code == 200
     assert len(response.json()['transactions']) > 0
     assert len(response.json()['transactions'][0]['category']) > 0
     assert len(response.json()['transactions'][0]['memo']) > 0
     assert len(response.json()['transactions'][0]['amount']) > 0
     assert len(response.json()['transactions'][0]['date']) > 0
-    

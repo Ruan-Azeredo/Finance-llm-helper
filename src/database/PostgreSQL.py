6c911d0e23875785d665791635165379f21a4540
Ruan-M2 <ruanazeredo@gmail.com>
2025-01-09 20:28:47 -0300

    :construction: build: define db environment default values (according to docker compose)

--- src/database/PostgreSQL.py
+++ src/database/PostgreSQL.py
@@ -9,4 +9,10 @@ POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 
-db = PostgresqlDatabase(POSTGRES_DB, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host=POSTGRES_HOST, port=5432)
+db = PostgresqlDatabase(
+    POSTGRES_DB or "postgres",
+    user = POSTGRES_USER or "postgres",
+    password = POSTGRES_PASSWORD or "postgres",
+    host = POSTGRES_HOST or "db",
+    port = 5432
+)

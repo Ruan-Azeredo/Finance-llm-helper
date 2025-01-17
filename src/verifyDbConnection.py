c021be03a570546a43b41f389d08370246bc3c86
Ruan-M2 <ruanazeredo@gmail.com>
2025-01-17 00:45:25 -0300

    :sparkles: feat: show db tables in verifyDbConnection

--- src/verifyDbConnection.py
+++ src/verifyDbConnection.py
@@ -4,6 +4,8 @@ def verifyDbConnection():
     try:
         db.connect()
         print("Conexão com o banco de dados bem-sucedida!")
+        tables = db.get_tables()
+        print(f"Tabelas: {tables}")
     except Exception as e:
         print(f"Erro ao conectar ao banco: {e}")
 

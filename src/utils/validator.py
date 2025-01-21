ef014f9b71b5a76a43ba7fada517d56f390827cb
Ruan-M2 <ruanazeredo@gmail.com>
2025-01-21 18:59:56 -0300

    :sparkles: feat: validators

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -1,5 +1,14 @@
+import re
+
 def category_validator(tags: list, category: str) -> bool:
     if category in tags and category is not None:
         return True
     else:
         return False
+    
+def is_valid_amount_format(amount_string: str) -> bool:
+    regex = r"^\d+,\d{2}$"
+    return bool(re.match(regex, amount_string))
+
+def is_valid_type_format(type_string: str) -> bool:
+    return type_string in ['expense', 'income']

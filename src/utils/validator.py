3c8d1ec23dedafc344485aa56b715d14aaeb44a6
Ruan-M2 <ruanazeredo@gmail.com>
2025-02-05 22:20:03 -0300

    :sparkles: feat: user tags operations

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -29,3 +29,45 @@ def validate_transaction_input(transaction: dict):
     
     if 'date' in transaction and transaction['date'] != None and is_valid_date_format(transaction['date']) is False:
         raise Exception(f'Formato de data inválido. Use dd/mm/aaaa. O formato recebido foi: {transaction["date"]}')
+    
+def is_valid_tags_format(tags: list) -> bool | str:
+    # tags = [{'name': 'Alimentação', 'color': 0}, {'name': 'Receitas', 'color': 1}]
+
+    print(tags)
+
+    if tags is None:
+        return False
+    
+    if len(tags) == 0:
+        return 'empty'
+    
+    tag_names = []
+    for tag in tags:
+
+        if 'name' not in tag or 'color' not in tag:
+            return False
+        
+        if type(tag['name']) is not str or type(tag['color']) is not int:
+            return False
+        
+        if tag['name'] == '':
+            return False
+        
+        if tag['name'] in tag_names:
+            # should not have repeated tags
+            return 'duplicate'
+
+        tag_names.append(tag['name'])
+
+    return True
+
+def validate_user_input(user: dict):
+
+    if 'tags' in user and user['tags'] != None and is_valid_tags_format(user['tags']) is not True:
+        if is_valid_tags_format(user['tags']) == 'duplicate':
+            raise Exception('Formato de tags está incorreto, tags não pode ter tags duplicadas.')
+        elif is_valid_tags_format(user['tags']) == 'empty':
+            raise Exception('Formato de tags está incorreto, tags não pode ser vazio.')
+        
+        raise Exception("Formato de tags está incorreto, tags devem ser do formato [{'name': 'str', 'color': int}]. "
+                        f"O tags recebido foi: {user['tags']}")
